f156128827ebd620b24b427332a3474027add7aa
st117169 <144341015+st117169@users.noreply.github.com>
2024-05-20 22:01:09 +0300

    Add files via upload

--- vector/Source.cpp
+++ vector/Source.cpp
@@ -10,10 +10,6 @@ class vector
 public:
 	vector() {}
 
-	vector(int* mass)
-	{
-		this->mass = mass;
-	}
 	vector(int n1)
 	{
 		this->n = n1;
@@ -23,10 +19,14 @@ public:
 			m[i] = 0;
 		}
 		this->mass = m;
+
+		m = new int[0];
+		delete[] m;
 	}
 
 	vector(const vector& a)
 	{
+		this->n = a.n;
 		this->mass = a.mass;
 	}
 
@@ -50,7 +50,10 @@ public:
 		}
 		m[this->n] = a;
 		this->mass = new int[this->n + 1];
-		this->mass = m; this->n += 1;
+		this->mass = m; 
+		this->n += 1;
+		m = new int[0];
+		delete[] m;
 
 	}
 	int indexof(int a)
@@ -84,9 +87,11 @@ public:
 		return this->mass[ind];
 	}
 
-	void operator= (vector a)
+	vector& operator= (vector a)
 	{
+		this->n = a.n;
 		this->mass = a.getmass();
+		return *this;
 	}
 
 	void remove1(int ind)
@@ -106,6 +111,9 @@ public:
 		this->n -= 1;
 		this->mass = new int[this->n];
 		this->mass = m;
+
+		m = new int[0];
+		delete[] m;
 	}
 
 	void remove(int a)
@@ -140,6 +148,9 @@ public:
 		this->n -= 1;
 		this->mass = new int[this->n];
 		this->mass = m;
+
+		m = new int[0];
+		delete[] m;
 	}
 
 	void print()
@@ -151,9 +162,173 @@ public:
 	}
 };
 
+class svector {
+
+	int n;
+	std::string* mass;
+
+public:
+	svector()
+	{}
+
+	svector(int n1)
+	{
+		this->n = n1;
+		this->mass = new std::string[n];
+		std::string* m = new std::string[n1];
+		for (int i = 0; i < n1; i++) {
+			m[i] = "";
+		}
+		this->mass = m;
+
+		m = new std::string[0];
+		delete[] m;
+	}
+
+	svector(const svector& a)
+	{
+		this->n = a.n;
+		this->mass = a.mass;
+	}
+
+	svector(svector&& a) noexcept
+	{
+		this->mass = a.mass;
+		delete[] a.mass;
+	}
+
+	~svector()
+	{
+		delete[] this->mass;
+	}
+
+	void push_back(std::string a)
+	{
+		std::string* m = new std::string[this->n + 1];
+		for (int i = 0; i < this->n; i++)
+		{
+			m[i] = this->mass[i];
+		}
+		m[this->n] = a;
+		this->mass = new std::string[this->n + 1];
+		this->mass = m;
+		this->n += 1;
+		m = new std::string[0];
+		delete[] m;
+
+	}
+	int indexof(std::string a)
+	{
+		for (int i = 0; i < this->n; i++)
+		{
+			if (this->mass[i] == a)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	int getsize()
+	{
+		return n;
+	}
+
+	std::string* getmass()
+	{
+		return this->mass;
+	}
+
+	std::string operator[](int ind)
+	{
+		if (ind < 0 || ind >= this->n)
+		{
+			svector a = svector();
+			return std::string();
+		}
+		return this->mass[ind];
+	}
+
+	svector& operator= (svector a)
+	{
+		this->n = a.n;
+		this->mass = a.getmass();
+		return *this;
+	}
+
+	void removeind(int ind)
+	{
+		std::string* m = new std::string[this->n - 1];
+		for (int i = 0; i < n; i++)
+		{
+			if (i < ind)
+			{
+				m[i] = this->mass[i];
+			}
+			if (i > ind)
+			{
+				m[i - 1] = this->mass[i];
+			}
+		}
+		this->n -= 1;
+		this->mass = new std::string[this->n];
+		this->mass = m;
+
+		m = new std::string[0];
+		delete[] m;
+	}
+
+	void removeel(std::string a)
+	{
+		std::string* m = new std::string[this->n - 1];
+		int k = -1;
+		for (int i = 0; i < this->n; i++)
+		{
+			if (this->mass[i] == a)
+			{
+				k = i;
+			}
+		}
+		if (k == -1)
+		{
+			return;
+		}
+		else
+		{
+			for (int i = 0; i < n; i++)
+			{
+				if (i < k)
+				{
+					m[i] = this->mass[i];
+				}
+				if (i > k)
+				{
+					m[i - 1] = this->mass[i];
+				}
+			}
+		}
+		this->n -= 1;
+		this->mass = new std::string[this->n];
+		this->mass = m;
+
+		m = new std::string[0];
+		delete[] m;
+	}
+
+	void print()
+	{
+		for (int i = 0; i < this->n; i++) {
+			std::cout << this->mass[i] << " ";
+		}
+		std::cout << std::endl;
+	}
+};
+
+
+
 int main()
 {
-	int a;
+	/*int a;
 	std::cin >> a;
 	vector v = vector();
 	for (int i = 0; i < a; i++)
@@ -168,4 +343,22 @@ int main()
 	int c; std::cin >> c;
 	v.remove(c);
 	v.print();
+	*/
+
+	int a;
+	std::cin >> a;
+	svector v = svector();
+	for (int i = 0; i < a; i++)
+	{
+		std::string h; std::cin >> h;
+		v.push_back(h);
+	}
+	v.print();
+	std::string c; std::cin >> c;
+	v.removeel(c);
+	v.print();
+	int b; std::cin >> b;
+	v.removeind(b);
+	v.print();
+
 }
