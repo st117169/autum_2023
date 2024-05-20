39ec43b23aed72b8cca4efd60d1101bd28c6b488
st117169 <144341015+st117169@users.noreply.github.com>
2024-05-20 12:21:47 +0300

    Delete pr directory

--- pr/ex.cpp
+++
@@ -1,9 +0,0 @@
-#include "ex.h"
-
-FileNotFoundException::FileNotFoundException() {}
-
-FileNotFoundException::FileNotFoundException(const std::string& message) : message(message) {}
-
-const char* FileNotFoundException::what() const noexcept {
-    return this->message.c_str();
-}
