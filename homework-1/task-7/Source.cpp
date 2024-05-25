ae5e5f25090235e81e6904d598d23de703843f5f
st117169 <st117169@student.spbu.ru>
2024-05-25 19:04:13 +0300

    autum_2023

--- homework-1/task-7/Source.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-	int a; 
-	std :: cin >> a;
-	std :: cout << "The next number for the number " << a << " " << "is " << (a + 1) << std :: endl << "The previous number for the number" << a
-		<< " is " << (a - 1) << std::endl;
-
-}
--- pr1/split.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <iostream>
-#include <vector>
-
-std::vector <std::string> split(std::string s); 
